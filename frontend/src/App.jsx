f254663bb0d610e7554f5a7989e33bee4efc533f
Pallavi Hegde <pallavighegde16@gmail.com>
2025-08-10 19:15:06 +0530

    custom hook add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,22 +1,25 @@
 import React from 'react'
-import {  Route, Routes } from 'react-router';
+import {  Navigate, Route, Routes } from 'react-router';
 import HomePage from "./pages/HomePage.jsx";
-import SignUpPage from './pages/SignUpPage.jsx';
+import SignUpPage from './pages/SignupPage.jsx';
 import Layout from './components/Layout.jsx';
 import LoginPage from "./pages/LoginPage.jsx";
 import { Toaster } from 'react-hot-toast';
 import QueuemanagementPage from './pages/QueuemanagementPage.jsx';
 import AppointmentPage from './pages/AppointmentPage.jsx'
+import useAuthUser from './hooks/useAuthUser.js';
 
 
 const App = () => {
-  
+
+  const { isLoading, isAuthenticated } = useAuthUser();
+
   return (
     <div data-theme="night" className='h-screen' >
       <Routes>
-        <Route path='/' element={<Layout><HomePage/></Layout>}/>
-        <Route path='/signup' element={<SignUpPage/>}/>
-        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/' element={isAuthenticated?(<Layout><HomePage/></Layout>):(<Navigate to="/login"/>)}/>
+        <Route path='/signup' element={!isAuthenticated ? <SignUpPage/> : <Navigate to="/"/>}/>
+        <Route path='/login' element={!isAuthenticated ? <LoginPage/> :<Navigate to="/"/>}/>
         <Route path='/queue' element={<Layout><QueuemanagementPage/></Layout>}/>
         <Route path='/appointment' element={<Layout><AppointmentPage/></Layout>}/>
       </Routes>
